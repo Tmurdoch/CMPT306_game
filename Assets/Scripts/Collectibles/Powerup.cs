9b2bb11db2f8278c00643e3928a45ec33b7db51a
Thomas <tjm149@usask.ca>
2022-11-04 19:27:38 -0600

    SAVED SCENE, gamemanager

--- Assets/Scripts/Collectibles/Powerup.cs
+++ Assets/Scripts/Collectibles/Powerup.cs
@@ -6,7 +6,10 @@ public class Powerup : MonoBehaviour
 {
     void onTriggerEnter(Collider other)
     {
+        if (other.transform.tag == "Player") {
+        Debug.Log("Collision");
         other.GetComponent<Player>().increaseSpeed();
         Destroy(gameObject);
+        }
     }
 }
