481ae017906e7a73cada4bad287eb1ae56a38b32
tjm149 <tjm149@usask.ca>
2022-12-06 20:05:34 -0600

    changed initial spawn time lower

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject Enemy;
-    private float spawnRate = 3.0f;
+    private float spawnRate = 2.5f;
     private float spawnTimer;
 
 
